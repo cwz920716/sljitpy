31d3b61db99c7bbea832e3745458b70ec678af79
cwz920716 <cwz920716@gmail.com>
2017-03-08 02:00:26 -0600

    ...

--- sljit/Lir.py
+++ sljit/Lir.py
@@ -9,6 +9,9 @@ import llvmlite.llvmpy.passes as lp
 
 from . import cgutils 
 
+def int32(i):
+    return ir.Constant(cgutils.int32_t, i)
+
 class LLVMCodeGenerator(object):
     def __init__(self):
         self.module = ir.Module()
@@ -20,7 +23,7 @@ class LLVMCodeGenerator(object):
     def _emit_prototype(self, name):
         # emit the prototype
         func_name = name
-        func_ty = ir.FunctionType(cgutils.int32_t, [])
+        func_ty = ir.FunctionType(cgutils.int32_t, [cgutils.int32_t])
         func = ir.Function(self.module, func_ty, func_name)
         return func
 
@@ -30,11 +33,44 @@ class LLVMCodeGenerator(object):
         builder.position_at_start(self.builder.function.entry_basic_block)
         return builder.alloca(ty, size=None, name=name)
 
+    def alloca_once(self, name, ty):
+        return cgutils.alloca_once(self.builder, ty, name=name)
+
     def emit_enter(self, name):
         self.func_symtab = {}
         self.func = self._emit_prototype(name)
         bb_entry = self.func.append_basic_block('entry')
         self.builder = ir.IRBuilder(bb_entry)
+        return
+
+    def emit_store(self, dst, src):
+        assert cgutils.is_pointer(dst.type)
+        self.builder.store(src, dst)
+        return
+
+    def emit_load(self, src, dst_name=''):
+        assert cgutils.is_pointer(src.type)
+        return self.builder.load(src, name=dst_name)
+
+    """
+        MOV supports following mode:
+          MOV R1, immd -- supported by MOVI
+          MOV R1, R2
+          MOV mem, R1
+          MOV mem, immd -- supported by MOVI
+          MOV R1, mem
+        mem supports following addressing mode:
+          [r1], [r1 + immd], [r1 + (r2 << immd)]
+    """
+    def emit_MOV(self, dst, src, ty1=None, ty2=None):
+        """
+           even if dst and src are two registers in Lir, they are modeled as allocated stack slot in LLVM, i.e., a pointer
+        """
+        assert cgutils.is_pointer(dst.type) and cgutils.is_pointer(src.type) 
+        if ty1 is None and ty2 is None:
+            val = self.emit_load(src, dst_name='mov_src')
+            self.emit_store(dst, val)
+            return
         return
 
     def emit_exit(self, retval):
@@ -51,14 +87,14 @@ class LirEvaluator(object):
         self.codegen = LLVMCodeGenerator()
         self.target = llvm.Target.from_default_triple()
 
-    def jit(self, name, optimize=True, llvmdump=True):
+    def jit(self, name, *args, optimize=True, llvmdump=True):
         if llvmdump:
             print('======== Unoptimized LLVM IR')
             print(str(self.codegen.module))
         llvmmod = llvm.parse_assembly(str(self.codegen.module))
         target_machine = self.target.create_target_machine(opt=3)
         if optimize:
-            pmb = lp.create_pass_manager_builder(opt=3)
+            pmb = lp.create_pass_manager_builder(opt=2)
             pm = llvm.create_module_pass_manager()
             pmb.populate(pm)
             pm.run(llvmmod)
@@ -68,5 +104,5 @@ class LirEvaluator(object):
         with llvm.create_mcjit_compiler(llvmmod, target_machine) as ee:
             ee.finalize_object()
             fptr = CFUNCTYPE(c_int)(ee.get_function_address(name))
-            result = fptr()
+            result = fptr(*args)
             return result   
--- tests/test_basics.py
+++ tests/test_basics.py
@@ -5,5 +5,9 @@ import sljit.cgutils as cg
 
 lir = Lir.LirEvaluator()
 lir.codegen.emit_enter('basic')
-lir.codegen.emit_exit( ir.Constant(cg.int32_t, 12345678) )
-assert lir.jit('basic') == 12345678
+r_curr_state = lir.codegen.alloca_once('R_CURR_STATE', cg.int32_t)
+r_next_state = lir.codegen.alloca_once('R_NEXT_STATE', cg.int32_t)
+lir.codegen.emit_store(r_next_state, lir.codegen.func.args[0] )
+lir.codegen.emit_MOV(r_curr_state, r_next_state )
+lir.codegen.emit_exit( lir.codegen.emit_load(r_curr_state) )
+assert lir.jit('basic', 12345678) == 12345678
